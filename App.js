50f12b38c2cfa2b70a4a6a58f0da253ef16fdf25
miql <mxander@gmail.com>
2015-12-01 18:01:32 -0700

    render method can only return a single node

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-// class compoent - can have state
-// class App extends React.Component {
-//   render() {
-//     return <div>Hello</div>
-//   }
-// }
+class App extends React.Component {
+  // render  method can only return a single node
+  render() {
+    return (
+      <div>
+        <div>Hello</div>
+        <b>hello</b>
+      </div>
+    )
+  }
+}
 
-// stateless function component
-// cannot have state
-
-const App = () => <h1>Hello, Egghead</h1>
 
 export default App;
